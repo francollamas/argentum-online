dba100c95bc0ea8058d289b9e675858e2d973b14
Franco Llamas <francollamas077@gmail.com>
2023-07-27 01:23:14 -0300

    Changes

---
+++ electron/build/capacitor.config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    appId: 'com.francollamas.argentumonline',
+    appName: 'Argentum Online',
+    webDir: 'dist',
+    server: {
+        androidScheme: 'https',
+    },
+};
+exports.default = config;
